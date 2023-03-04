cdadf0bccb0ab081f2a3cc23a376867fbf243ff9
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-03-05 01:18:25 +0300

    Create CustomerManagerMain.java

---
+++ JavaDemos/CustomerManager/CustomerManagerMain.java
@@ -0,0 +1,15 @@
+public class CustomerManagerMain {
+
+	public static void main(String[] args) {
+		
+		CustomerManager customerManager = new CustomerManager();
+		
+		customerManager.add();
+		
+		customerManager.delete();
+		
+		customerManager.update();
+		
+	}
+
+}
