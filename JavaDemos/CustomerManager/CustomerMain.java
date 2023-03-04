d463099b105ce6287352a7c110e9992242ce1d5c
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-03-05 01:17:49 +0300

    Create CustomerMain.java

---
+++ JavaDemos/CustomerManager/CustomerMain.java
@@ -0,0 +1,14 @@
+
+public class CustomerMain {
+
+	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+		Employee employee = new Employee();
+
+		CustomerManager customerManager = new CustomerManager();
+		EmployeeManager employeeManager = new EmployeeManager();
+
+	}
+
+}
