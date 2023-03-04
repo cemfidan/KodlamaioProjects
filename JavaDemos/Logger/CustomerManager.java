fb521ed5f455b96ab31cbeae744faafad6161999
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-03-05 01:45:14 +0300

    Add files via upload

---
+++ JavaDemos/Logger/CustomerManager.java
@@ -0,0 +1,15 @@
+
+public class CustomerManager  {
+	
+	private BaseLogger logger;
+	
+	public CustomerManager(BaseLogger logger) {
+		this.logger = logger;
+	}
+	
+	public void add() {
+		System.out.println("Customer added");
+		logger.log("Log message");
+	}
+
+}
