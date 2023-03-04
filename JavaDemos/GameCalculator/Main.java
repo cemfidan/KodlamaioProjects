eb4bfbf75d8b1e3a5ce1c387188f817f3aab3043
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-03-05 02:10:09 +0300

    Create Main.java

---
+++ JavaDemos/GameCalculator/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		WomanGameCalculator womanGameCalculator = new WomanGameCalculator();
+		womanGameCalculator.calculator();
+		womanGameCalculator.gameOver();
+
+		GameCalculator gameCalculator = new WomanGameCalculator();
+
+	}
+}
