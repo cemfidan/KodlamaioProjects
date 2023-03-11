f33110e831d07deda27a27340533a5fed8d8e169
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2023-03-12 01:15:32 +0300

    Update InMemoryLanguageRepository.java

--- devs/src/main/java/kodlama/io/devs/dataAccess/concretes/InMemoryLanguageRepository.java
+++ devs/src/main/java/kodlama/io/devs/dataAccess/concretes/InMemoryLanguageRepository.java
@@ -3,9 +3,12 @@ package kodlama.io.devs.dataAccess.concretes;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import kodlama.io.devs.dataAccess.abstracts.LanguageRepository;
 import kodlama.io.devs.entities.concretes.Language;
 
+@Repository
 public class InMemoryLanguageRepository implements LanguageRepository {
 
 	List<Language> languages;
